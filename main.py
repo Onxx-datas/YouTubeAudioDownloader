2a03feab3c9408ce0f3cf383108e267e4b7309c9
Onxx-datas <abdulazizkomiljonov4621@gmail.com>
2025-04-01 14:16:08 +0500

    Normal working .exe VER:1.0.0

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import os
 import sys
 import subprocess
-import yt_dlp
-import socket
-import re
-import urllib
-from download import DownloadThread
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QFileDialog
-from PyQt6.QtCore import QThread, pyqtSignal
-from PyQt6.QtGui import QCursor
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QFileDialog, QVBoxLayout, QWidget
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QCursor, QIcon
 
 output_folder = os.path.join(os.path.expanduser("~"), "Desktop", "Musics")
 os.makedirs(output_folder, exist_ok=True)
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.init_ui()
-        with open("style.qss", "r") as f:
+
+        icon_path = os.path.abspath("logo.ico")
+        self.setWindowIcon(QIcon(icon_path))
+
+        if getattr(sys, 'frozen', False):
+            base_path = sys._MEIPASS
+        else:
+            base_path = os.path.dirname(os.path.abspath(__file__))
+        style_path = os.path.join(base_path, 'style.qss')
+        with open(style_path, "r") as f:
             self.setStyleSheet(f.read())
 
     def init_ui(self):
@@ -26,37 +29,36 @@ class MainWindow(QWidget):
         self.setGeometry(100, 100, 850, 550)
         self.setFixedSize(850, 550)
 
-        # URL Input
+        layout = QVBoxLayout()
+        central_widget = QWidget(self)
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
         self.url_label = QLabel("Enter YouTube Video URL:", self)
-        self.url_label.setObjectName("urlLabelText")
-        self.url_label.move(70, 50)
+        self.url_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        self.url_label.setGeometry(70, 50, 600, 30)
 
         self.url_input = QLineEdit(self)
         self.url_input.setObjectName("urlInput")
         self.url_input.setGeometry(70, 80, 600, 30)  # x, y, width, height
 
-        # Link Opener Button
         self.open_links_button = QPushButton("Open links.txt", self)
         self.open_links_button.setObjectName("linkButton")
         self.open_links_button.setGeometry(70, 130, 115, 35)
 
-        # Download Button
         self.download_button = QPushButton("Download MP3", self)
         self.download_button.setObjectName("downloadButton")
         self.download_button.setGeometry(210, 130, 610, 35)
 
-        # Folder Selection
         self.folder_button = QPushButton("Saving folder", self)
         self.folder_button.setObjectName("folderButton")
         self.folder_button.setGeometry(645, 130, 115, 35)
 
-        # Status Label
         self.status_label = QLabel("Waiting for input...", self)
         self.status_label.setObjectName("statusLabel")
         self.status_label.setGeometry(70, 230, 400, 50)
         self.status_label.setWordWrap(True)
 
-        # Connect buttons to functions
         self.open_links_button.clicked.connect(self.open_links_file)
         self.download_button.clicked.connect(self.start_download)
         self.folder_button.clicked.connect(self.select_folder)
@@ -79,28 +81,37 @@ class MainWindow(QWidget):
 
     def start_download(self):
         self.download_button.setEnabled(False)
+        link = self.url_input.text().strip()
 
-        file_path = "links.txt"
+        if link:
+            links = [link]
+        else:
+            file_path = "links.txt"
+            MAX_LINKS = 12
 
-        if not os.path.exists(file_path):
-            self.status_label.setText("Error: links.txt not found.")
-            self.download_button.setEnabled(True)
-            return
+            if not os.path.exists(file_path):
+                self.status_label.setText("Error: links.txt not found.")
+                self.download_button.setEnabled(True)
+                return
 
-        with open(file_path, "r") as f:
-            links = [line.strip() for line in f if line.strip()]
+            with open(file_path, "r") as f:
+                links = [line.strip() for line in f if line.strip()]
 
-        if not links:
-            self.status_label.setText("No links found in links.txt.")
-            self.download_button.setEnabled(True)
-            return
+            if not links:
+                self.status_label.setText("No links found in links.txt.")
+                self.download_button.setEnabled(True)
+                return
+            if len(links) > MAX_LINKS:
+                self.status_label.setText(f"Error: Too many links! Limit is {MAX_LINKS}")
+                self.download_button.setEnabled(True)
+                return
 
         self.status_label.setText("Starting downloading...")
 
         self.threads = []
 
         for link in links:
-            download_thread = DownloadThread(link, output_folder)
+            download_thread = self.DownloadThread(link, output_folder)
             download_thread.progress_signal.connect(self.status_label.setText)
             download_thread.finished.connect(self.check_all_downloads_done)
             download_thread.start()
@@ -119,6 +130,8 @@ class MainWindow(QWidget):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    icon_path = os.path.abspath("logo.ico")
+    app.setWindowIcon(QIcon(icon_path))
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
